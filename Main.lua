58c113b9ddab9910e1e7f0f73b063b9c977690cf
bioeggs-hd <bagars192@gmail.com>
2025-07-06 07:51:00 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -13,8 +13,8 @@ local PGui = Player:WaitForChild("PlayerGui")
 local Ui
 local BgFrame
 local frame = {
-  Size = {0, 0},
-  Pos = {0.5, 0.5},
+  Size = {0.5, 0.5},
+  Pos = {0.25, 0.25},
   Col = {90, 90, 90},
   Trans = 0.15
 }
@@ -36,7 +36,6 @@ function SetupUi()
   Uis.Thickness = 4
 
   Ui.Enabled = true
-  BgFrame:TweenSizeAndPosition(UDim2.fromScale(0.5, 0.5), UDim2.fromScale(0.25, 0.25), Enum.EasingDirection.Out, Enum.EasingStyle.Quad, 0.5)
-end
+  Ts:Create(BgFrame, TweenInfo.new(0.5, Enum.EasingStyle.Quad, Enum.EasingDirection.Out, {Size = UDim2.fromScale(frame.Size[1], frame.Size[2]), Position = UDim2.fromScale(frame.Pos[1], frame.Pos[2])}):Play()
 
 SetupUi()
